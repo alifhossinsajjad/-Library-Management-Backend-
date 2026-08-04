5549ba6124811329c3e9f2cd098a88475981fc02
alifhossinsajjad <alifhossinsajjad123456@gmail.com>
2026-08-04 11:55:23 +0600

    project set up complete

---
+++ src/server.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+import { Server } from "node:http";
+import dotenv from "dotenv";
+import app from "./app";
+import dns from "dns"
+
+dotenv.config()
+
+dns.setServers(["8.8.8.8", "8.8.4.4"])
+
+let server: Server;
+const PORT = process.env.PORT;
+
+async function main() {
+  try {
+    await mongoose.connect(process.env.DATABASE_URL as string);
+    console.log("Connected to Database successfully");
+    server = app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    })
+    
+  } catch (error) {
+    console.log("Failed to connect to database", error);
+  }
+}
+
+main();
